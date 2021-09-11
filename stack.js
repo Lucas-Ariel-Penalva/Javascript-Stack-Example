38754ba30df6fa7fa9136fc13a531ac1266fc918
Lucas Ariel Peñalva <lucasarielpenalva@gmail.com>
2021-09-11 19:55:33 -0300

    Update stack.js

--- stack.js
+++ stack.js
@@ -17,9 +17,10 @@ class Stack{
             return undefined;
         };
         this.count--;
-        let result = this.stackData[this.count];
+        const result = this.stackData[this.count];
         delete this.stackData[this.count];
-        console.log(`Deleted ${result}.`);  
+        console.log(`Deleted ${result}.`);
+        return result;
     };
 
     size(){
@@ -28,7 +29,7 @@ class Stack{
     };
 
     peek(){
-        let topElement = this.stackData[this.count-1];
+        const topElement = this.stackData[this.count-1];
         console.log(`On top of the stack is ${topElement}.`);
         return topElement;
     };
@@ -36,13 +37,12 @@ class Stack{
     contains(element){
         for (let index in this.stackData) {
             if (this.stackData[index] === element){
-                console.log("Element present in the stack.")
+                console.log("The element is present in the stack.")
                 return true;
             }
         }
-        console.log("Element is not in the stack.")
+        console.log("The element is not in the stack.")
         return false;
     }
 
 };
-
